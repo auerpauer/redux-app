dfdd09d3d686354d4980294e76a869cac8079a51
Jim Auer <james.auer@yale.edu>
2020-04-05 12:36:50 -0400

    redux setup complete

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,38 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  // Arrow functions inherit the binding context of their enclosing scope.
+  // This is b/c arrow functions do not have a "this" object bound inside them.
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault;
+    alert(this.state.course.title);
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          //onChange={this.handleChange.bind(this)} this does NOT allow typing in the component
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
