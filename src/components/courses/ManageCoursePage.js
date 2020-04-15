87fbeb513b9e9d20c501497ccdc9a1448c1ef407
Jim Auer <james.auer@yale.edu>
2020-04-15 11:21:53 -0400

    state lost on course save

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
+import { loadAuthors } from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
+
+function ManageCoursePage({
+  courses,
+  authors,
+  loadCourses,
+  loadAuthors,
+  saveCourse,
+  history,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch((error) => {
+        // "catch" errors from the "promise"
+        alert("Loading Courses Failed " + error);
+      });
+    } else {
+      setCourse({ ...props.course });
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch((error) => {
+        // "catch" errors from the "promise"
+        alert("Loading Authors Failed " + error);
+      });
+    }
+  }, [props.course]); // empty array of objects to check means "only run once", like ComponentDidMount()
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse((prevCourse) => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value,
+    }));
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    saveCourse(course).then(() => {
+      history.push("/courses"); //doesn't carry state to next component
+    });
+  }
+
+  return (
+    <CourseForm
+      course={course}
+      authors={authors}
+      errors={errors}
+      onChange={handleChange}
+      onSave={handleSave}
+    />
+  );
+}
+
+// specify what sh/d be passed in; "dispatch" is passed in by connect as a default for "mapDispathToProps"
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
+};
+
+// determine props recv'd by child components
+function mapStateToProps(state) {
+  return {
+    course: newCourse,
+    courses: state.courses,
+    authors: state.authors,
+  }; // request only the data you component needs to reduce needless refreshes
+}
+
+const mapDispatchToProps = {
+  loadCourses,
+  loadAuthors,
+  saveCourse,
+};
+
+// "mapDispatchToProps" defaults to a dispatch property...whatever that means
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage); // "connect()" returns a function, which takes "ManageCoursePage" as it's argument
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,13 +1,19 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
-}
-
+// ************************
 // functions used by thunks
+// ************************
 function loadCourseSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+function updateCourseSuccess(courses) {
+  return { type: types.UPDATE_COURSE_SUCCESS, courses };
+}
+
+function createCourseSuccess(courses) {
+  return { type: types.CREATE_COURSE_SUCCESS, courses };
 }
 
 // thunks
@@ -17,6 +23,22 @@ export function loadCourses() {
       .getCourses()
       .then((courses) => {
         dispatch(loadCourseSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    // "getState" accesses the Redux store
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
       })
       .catch((error) => {
         throw error;
